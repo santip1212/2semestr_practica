6f4d6a418fc80425aef8b1e20980069b40b528cc
santip1212 <serg1862014@gmail.com>
2026-07-08 22:27:26 +0700

    Удалены служебные файлы и папки сборки

--- build/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_ImageCompressor_FILE C:/2semestr_practica/build/ImageCompressor.exe)
-set(__QT_DEPLOY_TARGET_ImageCompressor_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_ImageCompressor_RUNTIME_DLLS C:/msys64/ucrt64/bin/Qt6Widgets.dll;C:/msys64/ucrt64/bin/Qt6Gui.dll;C:/msys64/ucrt64/bin/Qt6Core.dll)
